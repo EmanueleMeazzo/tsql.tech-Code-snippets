d1f83a4a0376bc31a2d85bd602a9aa58547d0d93
Emanuele Meazzo <619619.psp@gmail.com>
2019-03-27 17:12:58 +0100

    Update Find Active Locks.sql

--- DMV/Find Active Locks.sql
+++ DMV/Find Active Locks.sql
@@ -1,35 +1,47 @@
-SELECT CASE [DTL].[request_session_id]
-           WHEN-2 THEN 'ORPHANED DISTRIBUTED TRANSACTION'
-           WHEN-3 THEN 'DEFERRED RECOVERY TRANSACTION'
-           ELSE [DTL].[request_session_id]
-       END AS [SPID],
-       DB_NAME([DTL].[resource_database_id]) AS [DATABASENAME],
-       [SO].[name] AS [LOCKEDOBJECTNAME],
-       [DTL].[resource_type] AS [LOCKEDRESOURCE],
-       [DTL].[resource_description] AS [RESOURCEDESCRIPTION],
-       [DTL].[REQUEST_MODE] AS [LOCKTYPE],
-       [ST].TEXT AS [SQLSTATEMENTTEXT],
-       [ES].[LOGIN_NAME] AS [LOGINNAME],
-       [ES].HOST_NAME AS [HOSTNAME],
-       CASE [TST].[IS_USER_TRANSACTION]
-           WHEN 0 THEN 'SYSTEM TRANSACTION'
-           WHEN 1 THEN 'USER TRANSACTION'
-       END AS [USER_OR_SYSTEM_TRANSACTION],
-       [AT].[name] AS [TRANSACTIONNAME],
-       [DTL].[REQUEST_STATUS]
-FROM   SYS.DM_TRAN_LOCKS AS DTL
-JOIN SYS.PARTITIONS AS SP
-ON SP.HOBT_ID = DTL.RESOURCE_ASSOCIATED_ENTITY_ID
-JOIN SYS.OBJECTS AS SO
-ON SO.OBJECT_ID = SP.OBJECT_ID
-JOIN SYS.DM_EXEC_SESSIONS AS ES
-ON ES.SESSION_ID = DTL.REQUEST_SESSION_ID
-JOIN SYS.DM_TRAN_SESSION_TRANSACTIONS AS TST
-ON ES.SESSION_ID = TST.SESSION_ID
-JOIN SYS.DM_TRAN_ACTIVE_TRANSACTIONS AT
-ON TST.TRANSACTION_ID = AT.TRANSACTION_ID
-JOIN SYS.DM_EXEC_CONNECTIONS AS EC
-ON EC.SESSION_ID = ES.SESSION_ID
-OUTER APPLY SYS.DM_EXEC_SQL_TEXT(EC.MOST_RECENT_SQL_HANDLE) AS ST
-WHERE  [RESOURCE_DATABASE_ID] = DB_ID()
-ORDER BY [DTL].[REQUEST_SESSION_ID];
+-- List all Locks of the Current Database 
+SELECT TL.resource_type AS ResType 
+      ,TL.resource_description AS ResDescr 
+      ,TL.request_mode AS ReqMode 
+      ,TL.request_type AS ReqType 
+      ,TL.request_status AS ReqStatus 
+      ,TL.request_owner_type AS ReqOwnerType 
+      ,TAT.[name] AS TransName 
+      ,TAT.transaction_begin_time AS TransBegin 
+      ,DATEDIFF(ss, TAT.transaction_begin_time, GETDATE()) AS TransDura 
+      ,ES.session_id AS S_Id 
+      ,ES.login_name AS LoginName 
+      ,COALESCE(OBJ.name, PAROBJ.name) AS ObjectName 
+      ,PARIDX.name AS IndexName 
+      ,ES.host_name AS HostName 
+      ,ES.program_name AS ProgramName 
+      ,ST.Text as SQLStatementText
+FROM sys.dm_tran_locks AS TL 
+     INNER JOIN sys.dm_exec_sessions AS ES 
+         ON TL.request_session_id = ES.session_id 
+     LEFT JOIN sys.dm_tran_active_transactions AS TAT 
+         ON TL.request_owner_id = TAT.transaction_id 
+            AND TL.request_owner_type = 'TRANSACTION' 
+     LEFT JOIN sys.objects AS OBJ 
+         ON TL.resource_associated_entity_id = OBJ.object_id 
+            AND TL.resource_type = 'OBJECT' 
+     LEFT JOIN sys.partitions AS PAR 
+         ON TL.resource_associated_entity_id = PAR.hobt_id 
+            AND TL.resource_type IN ('PAGE', 'KEY', 'RID', 'HOBT') 
+     LEFT JOIN sys.objects AS PAROBJ 
+         ON PAR.object_id = PAROBJ.object_id 
+     LEFT JOIN sys.indexes AS PARIDX 
+         ON PAR.object_id = PARIDX.object_id 
+            AND PAR.index_id = PARIDX.index_id 
+     LEFT JOIN sys.dm_exec_connections AS EC
+         ON EC.session_id = ES.session_id
+    OUTER APPLY sys.dm_exec_sql_text(EC.most_recent_sql_handle) AS ST
+WHERE TL.resource_database_id  = DB_ID() 
+      AND ES.session_id <> @@Spid -- Exclude "my" session 
+      -- optional filter  
+      AND TL.request_mode <> 'S' -- Exclude simple shared locks 
+ORDER BY TL.resource_type 
+        ,TL.request_mode 
+        ,TL.request_type 
+        ,TL.request_status 
+        ,ObjectName 
+        ,ES.login_name;
