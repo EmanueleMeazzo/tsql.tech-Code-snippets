4649d174b45dd9bdbef3f3a6eab251ddeb627e4b
EmanueleMeazzo <Emanuele.meazzo@gmail.com>
2019-10-07 23:02:20 +0200

    Added "How to use PowerBI as a Monitoring Tool" session materials

---
+++ Sessions Material/How to use PowerBI as a Monitoring Tool/Sample Reports/Streaming DataSet Demo.ps1
@@ -0,0 +1,23 @@
+﻿$i = 1
+
+while ($i -le 25) {
+    $endpoint = "https://api.powerbi.com/beta/yourendpointmaybe?"
+
+$machinename = Get-ComputerInfo -Property CsName
+$timestamp = Get-Date
+$freememorybytes = Get-Counter -Counter "\memory\available bytes"
+$totalprocessortime = Get-Counter -Counter "\Processor(_Total)\% Processor Time"
+
+$payload = @{
+"MachineName" = $machinename.CsName
+"TimeStamp" = $timestamp
+"FreeMemoryBytes" = $freememorybytes.CounterSamples.CookedValue
+"TotalProcessorTime" = $totalprocessortime.CounterSamples.CookedValue
+}
+
+Invoke-RestMethod -Method Post -Uri "$endpoint" -Body (ConvertTo-Json @($payload))
+
+Start-Sleep -Seconds 1
+
+$i++
+}
