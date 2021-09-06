512cb076e5a2951293ad3bb54783439bf28cf329
Emanuele Meazzo <619619.psp@gmail.com>
2021-09-06 15:17:21 +0200

    Update Rebuild Fragmented Heaps.sql
    Updated to work in all schemas

--- Maintenance/Rebuild Fragmented Heaps.sql
+++ Maintenance/Rebuild Fragmented Heaps.sql
@@ -1,30 +1,31 @@
 DECLARE C CURSOR LOCAL FAST_FORWARD
-FOR SELECT
-	OBJECT_NAME(object_id)
+FOR SELECT obj.name as TableName, schem.name as SchemaName
 FROM sys.dm_db_index_physical_stats(
-     DB_ID('Urban_PLANNING_AppDB')
+     DB_ID()
     ,NULL
     ,NULL
     ,NULL
-    ,'DETAILED')
-WHERE index_id = 0
+    ,'DETAILED') stats
+JOIN sys.objects obj ON obj.object_id = stats.object_id
+JOIN sys.schemas schem on schem.schema_id = obj.schema_id
+WHERE stats.index_id = 0
 AND avg_fragmentation_in_percent > 20;
 
 OPEN C
-DECLARE @TableName nvarchar(256)
+DECLARE @TableName sysname, @SchemaName sysname
 DECLARE @SQL nvarchar(max)
 
-FETCH NEXT FROM C INTO @TableName
+FETCH NEXT FROM C INTO @TableName, @SchemaName
 
 WHILE @@FETCH_STATUS = 0
 BEGIN
 
-SET @SQL = N'ALTER TABLE ' + QUOTENAME(@TableName) + N' REBUILD'
+SET @SQL = N'ALTER TABLE ' + QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName) + N' REBUILD'
 
 PRINT @SQL
 EXEC sp_executesql @SQL
 
-FETCH NEXT FROM C INTO @TableName
+FETCH NEXT FROM C INTO @TableName, @SchemaName
 
 END 
 
